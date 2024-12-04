b9fd0aecee0a3eab20cc4719ead44b59f0a06b9c
Anthony Suarez <asuarezop@gmail.com>
2024-12-04 09:49:53 -0500

    Added in .env and dotenv dependency to pom.xml + refactored SQL statement to use PreparedStatements

--- jdbc-mysql-demo/src/main/java/SQLMain.java
+++ jdbc-mysql-demo/src/main/java/SQLMain.java
@@ -1,41 +1,69 @@
+import io.github.cdimascio.dotenv.Dotenv;
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import java.sql.*;
+import java.util.Scanner;
+
 
 public class SQLMain {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/car_dealership";
-        String user = "root";
-        String password = "new_password";
+        Scanner inputSc = new Scanner(System.in);
+        Dotenv dotenv = Dotenv.configure().load();
 
-        try {
+        // -- Try catch with resources ()
+        /* Able to assign resources into try catch blocks. The advantage of this would be that a resource can be known throughout
+           the entire try catch block and automatically close/terminate resources for you.
+        * */
+        try (BasicDataSource dataSource = new BasicDataSource()) {
+
+            //Retrieving properties from .env file
+            String url = dotenv.get("DB_URL");
+            String user = dotenv.get("DB_USER");
+            String password = dotenv.get("DB_PASSWORD");
+
+            //Setting data source connection using .env values
+            dataSource.setUrl(url);
+            dataSource.setUsername(user);
+            dataSource.setPassword(password);
+
             //Establish a new connection to MySQL database
-            Connection conn = DriverManager.getConnection(url, user, password);
+            try (Connection conn = dataSource.getConnection()) {
 
-            Statement statement = conn.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM vehicles");
-            ResultSet resultSet2 = statement.executeQuery("""
-                    SELECT * FROM dealerships
-                    JOIN inventory ON inventory.dealership_id = dealerships.dealership_id,
-                    WHERE vin = 10003;
-                    """);
+                //A statement object that holds a SQL query (PreparedStatements always preferred to protect against SQL injection)
+                PreparedStatement selectStatement = conn.prepareStatement("SELECT * FROM vehicles WHERE make = ? ");
+//                PreparedStatement joinStatement = conn.prepareStatement("""
+//                        SELECT * FROM dealerships
+//                        JOIN inventory ON inventory.dealership_id = dealerships.dealership_id,
+//                        WHERE vin = 10003;
+//                        """);
 
-            while (resultSet.next()) {
-                int vinNum = resultSet.getInt("vin");
-                String vehicleMake = resultSet.getString("make");
-                String vehicleModel = resultSet.getString("model");
+                System.out.println("Enter vehicle make to search for: ");
+                String vehicleMake = inputSc.nextLine();
+                selectStatement.setString(1, vehicleMake);
 
-                double vehiclePrice = resultSet.getDouble("vehicle_price");
-                System.out.printf("%d, %s %s, %.2f \n", vinNum, vehicleMake, vehicleModel, vehiclePrice);
-            }
+                //Table that contains returned data from executing SQL statements
+                ResultSet resultSet = selectStatement.executeQuery();
+//                ResultSet resultSet2 = joinStatement.executeQuery();
 
-            while (resultSet2.next()) {
-                String name = resultSet2.getString("name");
-                String address = resultSet2.getString("address");
-                System.out.printf("%s, %s", name, address);
+                while (resultSet.next()) {
+                    int vin = resultSet.getInt("vin");
+                    String make = resultSet.getString("make");
+                    String model = resultSet.getString("model");
+                    double vehiclePrice = resultSet.getDouble("vehicle_price");
+                    System.out.printf("%d, %s %s, $%.2f\n", vin, make, model, vehiclePrice);
+                }
+
+//                while (resultSet2.next()) {
+//                    String name = resultSet2.getString("name");
+//                    String address = resultSet2.getString("address");
+//                    System.out.printf("%s, %s", name, address);
+//                }
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 }
